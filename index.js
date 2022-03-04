2e6cfbad9eff1099af475e7a7049bbdd30cfd710
CAS <78571139+cas-14@users.noreply.github.com>
2022-03-04 16:48:19 -0500

    does this still work

---
+++ index.js
@@ -0,0 +1,7 @@
+function select(a) {
+    items = document.querySelectorAll('.single.active');
+    if(items.length) {
+      items[0].className = 'single';
+    }
+    a.className = 'single active';
+}
