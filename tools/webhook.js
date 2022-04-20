c1b35235bb61ab4ebd2aa6020f2897fa75e04d5f
CAS <78571139+cas-14@users.noreply.github.com>
2022-04-20 11:59:17 -0400

    fix it

--- tools/webhook.js
+++ tools/webhook.js
@@ -1,10 +1,8 @@
 function send() {
     const request = new XMLHttpRequest();
-    request.open("POST", document.getElementById("webhook_url"));
+    request.open("POST", document.getElementById("webhook_url").value);
     request.setRequestHeader('Content-type', 'application/json');
-    const params = {
-        content: document.getElementById("message")
-    }
+    const params = {content: document.getElementById("message").value}
     request.send(JSON.stringify(params));
 
     if (document.getElementById("toggle_clear").checked) {
