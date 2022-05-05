98d307b8583b5e56250965d91f75d113e54cef24
CAS <78571139+cas-14@users.noreply.github.com>
2022-05-05 12:50:41 -0400

    funny

--- 404script.js
+++ 404script.js
@@ -1,4 +1,12 @@
-document.onkeypress = function (e) {
+const konami_code = "ArrowUp,ArrowUp,ArrowDown,ArrowDown,ArrowLeft,ArrowRight,ArrowLeft,ArrowRight,B,A,Enter,";
+var keys_pressed = "";
+
+document.onkeydown = (e) => {
     e = e || window.event;
-    console.log("CHEESED BURGER")
-};
+    keys_pressed = keys_pressed + e.key + ",";
+
+    if (keys_pressed.includes(konami_code)) {
+        window.alert("you've done the funny");
+        keys_pressed = "";
+    }
+}
