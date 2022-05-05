1a4e52b280c3844773606c6d4645e0628f751793
CAS <78571139+cas-14@users.noreply.github.com>
2022-05-05 12:52:06 -0400

    KEY!

--- 404script.js
+++ 404script.js
@@ -4,6 +4,7 @@ var keys_pressed = "";
 document.onkeydown = (e) => {
     e = e || window.event;
     keys_pressed = keys_pressed + e.key + ",";
+    console.log("Detected key: "+e.key)
 
     if (keys_pressed.includes(konami_code)) {
         window.alert("you've done the funny");
