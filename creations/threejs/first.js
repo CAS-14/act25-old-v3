a825cf30c2d2833971dcf118bb0a538152ae4951
CAS <78571139+cas-14@users.noreply.github.com>
2022-05-19 08:30:50 -0400

    fit content to div

--- creations/threejs/first.js
+++ creations/threejs/first.js
@@ -1,9 +1,10 @@
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+var camera = new THREE.PerspectiveCamera(75, container.clientWidth / container.clientHeight, 0.1, 1000);
+var container = document.getElementById("container")
 
 var renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+renderer.setSize(container.clientWidth, container.clientHeight);
+container.appendChild(renderer.domElement);
 
 var geometry = new THREE.BoxGeometry(1, 1, 1);
 var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
